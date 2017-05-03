88119610122c169ea0291d81ad963e31b20a68cd
Rong Li Lab <Rong Li Lab>
2017-05-03 17:48:13 -0400

    mouse brain 500 run

--- generateProbeList.m
+++ generateProbeList.m
@@ -22,7 +22,7 @@ uniqueHeader = unique(trimHeader, 'stable');
 indexTotal = zeros(length(trimHeader),1);
 for n = 1:length(uniqueHeader)
     index = ismember(trimHeader, uniqueHeader{n,1});
-    if sum(index) < params.number
+    if sum(index) < params.number && isempty(strfind(uniqueHeader{n,1},'ENSSPT')) % for Bin's special sequences
         indexTotal = indexTotal+index;
         if params.verbose
             disp(['  transcript ' uniqueHeader{n,1} ...
