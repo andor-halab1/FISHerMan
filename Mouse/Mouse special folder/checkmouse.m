b0f87028d8ba6593505371dc06b71c1588295e77
Rong Li Lab <Rong Li Lab>
2017-06-09 21:31:49 -0400

    checked old mouse probes:arna

--- Mouse/Mouse special folder/checkmouse.m
+++ Mouse/Mouse special folder/checkmouse.m
@@ -1,4 +1,4 @@
-[Header,Sequence]=fastaread('C:\FISHerMan\Mouse\Mouse.probes.txt');
+[Header,Sequence]=fastaread('C:\FISHerMan\Mouse\Mouse special folder\Mouse.probes.txt');
 Header=Header';
 Sequence=Sequence';
 
@@ -8,7 +8,7 @@ for n = 1:length(Sequence)
     tempSequence{n,1}=Sequence{n,1}(41:70);
 end
 
-[totalHeader,totalSequence]=fastaread('C:\FISHerMan\Mouse\Mouse.oligos.temp.txt');
+[totalHeader,totalSequence]=fastaread('C:\FISHerMan\Mouse\Mouse special folder\Mouse.oligos.temp.txt');
 totalHeader=totalHeader';
 totalSequence=totalSequence';
 
@@ -33,3 +33,37 @@ Header(indexTotal)=[];
 Sequence(indexTotal)=[];
 
 fastawrite('Mouse.probes.temp.txt',Header,Sequence);
+
+%% start over to blast against mouse abundant rna
+[Header,Sequence]=fastaread('C:\FISHerMan\Mouse\Mouse special folder\Mouse.probes.temp.txt');
+Header=Header';
+Sequence=Sequence';
+
+tempHeader=Header;
+tempSequence=Sequence;
+for n = 1:length(Sequence)
+    tempSequence{n,1}=Sequence{n,1}(21:90);
+end
+
+[Header,tempSequence,Sequence]...
+    =blastAbundantRNASimple(Header,tempSequence,Sequence,params.arna);
+
+uniqueHeader=unique(Header);
+
+indexTotal=ismember(Header,uniqueHeader{10,1});
+for n = 2:length(uniqueHeader)
+    index=ismember(Header,uniqueHeader{n,1});
+    if sum(index)<48 && isempty(strfind(uniqueHeader{n,1},'ENSSPT'))
+        indexTotal=indexTotal+index;
+    end
+end
+indexTotal=logical(indexTotal);
+
+Header(indexTotal)=[];
+Sequence(indexTotal)=[];
+
+fastawrite('C:\FISHerMan\Mouse\Mouse special folder\Mouse.probes.arna.txt',Header,Sequence);
+
+
+
+
