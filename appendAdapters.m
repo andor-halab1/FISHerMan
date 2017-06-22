96ceabcab516cc650630bcd6dc6132a6606b13e9
Rong Li Lab <Rong Li Lab>
2017-06-21 21:59:35 -0400

    made changes for designing probes with only 3 segments

--- appendAdapters.m
+++ appendAdapters.m
@@ -1,4 +1,6 @@
-function [adapterList,probeHeader,probeSequence,probeSequence3Seg,probeSequenceCore]...
+% function [adapterList,probeHeader,probeSequence,probeSequence3Seg,probeSequenceCore]...
+%     =appendAdapters(adapterList,oligos,params)
+function [adapterList,probeHeader,probeSequence,probeSequence2Seg,probeSequenceCore]...
     =appendAdapters(adapterList,oligos,params)
 
 % params = struct('species','Mouse','verbose',1,...
@@ -32,7 +34,8 @@ fastawrite(adapterList, adapterHeader, adapterSequence);
 
 probeHeader = {};
 probeSequence = {};
-probeSequence3Seg = {};
+% probeSequence3Seg = {};
+probeSequence2Seg = {};
 probeSequenceCore = {};
 for n = 1:length(adapterHeader)
     if params(1).verbose && mod(n, 1000) == 1
@@ -41,23 +44,28 @@ for n = 1:length(adapterHeader)
     index = ismember(trimmedHeader, adapterHeader{n,1});
     probeHeader(end+1:end+sum(index),1) = Header(index);
     probe = Sequence(index);
-    probe3Seg = Sequence(index);
+%     probe3Seg = Sequence(index);
+    probe2Seg = Sequence(index);
     probeCore = Sequence(index);
     for m = 1:length(probe)
         temp = probe{m,1};
-        temp3Seg = probe3Seg{m,1};
+%         temp3Seg = probe3Seg{m,1};
+        temp2Seg = probe2Seg{m,1};
         tempCore = probeCore{m,1};
         
         temp = strcat(params(1).gf, adapterSequence{n,1}, temp, params(1).grr);
-        temp3Seg = strcat(adapterSequence{n,1}, temp3Seg, params(1).grr, 'CCC');
+%         temp3Seg = strcat(adapterSequence{n,1}, temp3Seg, params(1).grr, 'CCC');
+        temp2Seg = strcat(adapterSequence{n,1}, temp2Seg, 'CCC');
         tempCore = strcat(adapterSequence{n,1}(11:20), tempCore, params(1).grr(1:10));
         
         probe{m,1} = temp;
-        probe3Seg{m,1} = temp3Seg;
+%         probe3Seg{m,1} = temp3Seg;
+        probe2Seg{m,1} = temp2Seg;
         probeCore{m,1} = tempCore;
     end
     probeSequence(end+1:end+sum(index),1) = probe;
-    probeSequence3Seg(end+1:end+sum(index),1) = probe3Seg;
+%     probeSequence3Seg(end+1:end+sum(index),1) = probe3Seg;
+    probeSequence2Seg(end+1:end+sum(index),1) = probe2Seg;
     probeSequenceCore(end+1:end+sum(index),1) = probeCore;
 end
 
