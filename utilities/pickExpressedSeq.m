2a6ab3e8c75fb27daf27a4c4b3d113ddd2db6c25
andorhalab1 <andorhalab1@gmail.com>
2017-10-06 15:02:10 -0400

    reverted changes made to pickExpressed Seq; checked rna-seq data

--- utilities/pickExpressedSeq.m
+++ utilities/pickExpressedSeq.m
@@ -1,41 +1,17 @@
 function [expressedHeader, expressedSequence] = pickExpressedSeq(seqData, Header, Sequence)
 
-if isempty(seqData{1,1})
-    transcriptID = seqData(:,2);
+transcriptID = seqData(:,1);
 
-    pos = regexp(Header, ':');
-    temp = Header;
-    index = [];
-    for n = 1:length(Header)
-        if length(pos{n,1}) == 2
-            temp{n,1} = Header{n,1}(pos{n,1}(1)+1:pos{n,1}(2)-1);
-        elseif length(pos{n,1}) == 1
-            temp{n,1} = Header{n,1}(pos{n,1}(1)+1:end);
-        else
-            disp('missing gene ID');
-            quit;
-        end
-        if sum(strcmp(transcriptID, temp{n,1})) ~= 0
-            index = [index;n];
-        end
+pos = regexp(Header, ':');
+temp = Header;
+for n = 1:length(Header)
+    if ~isempty(pos{n,1})
+        temp{n,1} = Header{n,1}(1:pos{n,1}(1)-1);
+    else
+        temp{n,1} = Header{n,1};
     end
-
-    expressedHeader = Header(index);
-    expressedSequence = Sequence(index);
-else
-    transcriptID = seqData(:,1);
-
-    pos = regexp(Header, ':');
-    temp = Header;
-    for n = 1:length(Header)
-        if ~isempty(pos{n,1})
-            temp{n,1} = Header{n,1}(1:pos{n,1}(1)-1);
-        else
-            temp{n,1} = Header{n,1};
-        end
-    end
-
-    [~,index,~] = intersect(temp, transcriptID, 'stable');
-    expressedHeader = Header(index);
-    expressedSequence = Sequence(index);
 end
+
+[~,index,~] = intersect(temp, transcriptID, 'stable');
+expressedHeader = Header(index);
+expressedSequence = Sequence(index);
