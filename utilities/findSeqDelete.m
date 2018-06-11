bf9a510db48f970b964b3de7b46fb7e9ae33857e
Tunc Kayikcioglu <kayikcioglut@gmail.com>
2018-06-11 12:51:12 -0400

    Update findSeqDelete.m

--- utilities/findSeqDelete.m
+++ utilities/findSeqDelete.m
@@ -1,42 +1,42 @@
 function seqDelete=findSeqDelete(data)
 
-%% Delete probes that non-specifically bind 2nd PCR primers
-seqDelete = [];
-for n = 1:length(data{2,1})
-    if ~isempty(data{2,1}(n).Hits)
-        seqDelete = [seqDelete;n];
+    %% Delete probes that non-specifically bind 2nd PCR primers
+    seqDelete = [];
+    for n = 1:length(data{2,1})
+        if ~isempty(data{2,1}(n).Hits)
+            seqDelete = [seqDelete;n];
+        end
     end
-end
 
-%% Delete probes that non-specifically bind other probes
-pairDelete = {};
-for n = 1:length(data{1,1})
-    for m = 1:length(data{1,1}(n).Hits)
-        pairDelete{end+1,1} = data{1,1}(n).Query;
-        pairDelete{end,2} = data{1,1}(n).Hits(m).Name;
+    %% Delete probes that non-specifically bind other probes
+    pairDelete = {};
+    for n = 1:length(data{1,1})
+        for m = 1:length(data{1,1}(n).Hits)
+            pairDelete{end+1,1} = data{1,1}(n).Query;
+            pairDelete{end,2} = data{1,1}(n).Hits(m).Name;
+        end
     end
-end
 
-pos = regexp(pairDelete,'=');
-pairNumber = {};
-for k = 1:size(pos,1)
-    temp1 = pairDelete{k,1}(pos{k,1}+1:end);
-    temp2 = pairDelete{k,2}(pos{k,2}+1:end);
-    temp1 = uint16(str2double(temp1));
-    temp2 = uint16(str2double(temp2));
-    pairNumber{end+1,1} = temp1;
-    pairNumber{end,2} = temp2;
-end
-pairNumber = cell2mat(pairNumber);
+    pos = regexp(pairDelete,'=');
+    pairNumber = {};
+    for k = 1:size(pos,1)
+        temp1 = pairDelete{k,1}(pos{k,1}+1:end);
+        temp2 = pairDelete{k,2}(pos{k,2}+1:end);
+        temp1 = uint16(str2double(temp1));
+        temp2 = uint16(str2double(temp2));
+        pairNumber{end+1,1} = temp1;
+        pairNumber{end,2} = temp2;
+    end
+    pairNumber = cell2mat(pairNumber);
 
-while size(pairNumber,1) > 0
-    temp = reshape(pairNumber, [numel(pairNumber),1]);
-    tempDelete = mode(temp);
-    seqDelete = [seqDelete;tempDelete];
-    index = (pairNumber == tempDelete);
-    index = logical(index(:,1)+index(:,2));
-    pairNumber(index,:) = [];
-end
+    while size(pairNumber,1) > 0
+        temp = reshape(pairNumber, [numel(pairNumber),1]);
+        tempDelete = mode(temp);
+        seqDelete = [seqDelete; tempDelete];
+        index = (pairNumber == tempDelete);
+        index = logical(index(:,1) + index(:,2));
+        pairNumber(index,:) = [];
+    end
 
-seqDelete = unique(seqDelete, 'stable');
+    seqDelete = unique(seqDelete, 'stable');
 
