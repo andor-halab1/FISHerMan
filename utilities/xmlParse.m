61bbaf8cd655484d792319128f1d405cc54194ed
Tunc Kayikcioglu <kayikcioglut@gmail.com>
2018-06-11 12:56:07 -0400

    Update xmlParse.m

--- utilities/xmlParse.m
+++ utilities/xmlParse.m
@@ -1,8 +1,8 @@
-function branch=xmlParse(root, rootName, branchName)
+function branch = xmlParse(root, rootName, branchName)
 
-try
-    branch = root.getElementsByTagName(branchName);
-    branch = branch.item(0);
-catch
-    disp(['error occurred at ' rootName]);
-end
+    try
+        branch = root.getElementsByTagName(branchName);
+        branch = branch.item(0);
+    catch
+        disp(['error occurred at ' rootName]);
+    end
