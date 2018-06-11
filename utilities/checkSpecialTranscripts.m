8c3344945820d94d07b576da3167a71e38b576d0
Tunc Kayikcioglu <kayikcioglut@gmail.com>
2018-06-11 12:45:06 -0400

    Update checkSpecialTranscripts.m

--- utilities/checkSpecialTranscripts.m
+++ utilities/checkSpecialTranscripts.m
@@ -1,14 +1,16 @@
-function notSpecialTranscript = checkSpecialTranscripts(transcriptName,params)
+function notSpecialTranscript = checkSpecialTranscripts (transcriptName,params)
+    % params = struct('species','Mouse','verbose',1,...
+    %     'specialTranscripts','C:\FISHerMan\Db\Mouse.STList.fas');
 
-% params = struct('species','Mouse','verbose',1,...
-%     'specialTranscripts','C:\FISHerMan\Db\Mouse.STList.fas');
+    [specialTranscripts,~] = fastaread(params(1).specialTranscripts);
+    specialTranscripts=specialTranscripts';
 
-[specialTranscripts,~]=fastaread(params(1).specialTranscripts);
-specialTranscripts=specialTranscripts';
-
-% transcriptName and specialTranscripts are one-segment Header
-notSpecialTranscript = true;
-for n = 1:length(specialTranscripts)
-    notSpecialTranscript = notSpecialTranscript && ...
-        ~strcmp(transcriptName,specialTranscripts{n,1});
+    % transcriptName and specialTranscripts are one-segment Header
+    notSpecialTranscript = true;
+    for n = 1:length(specialTranscripts)
+        if strcmp(transcriptName,specialTranscripts{n,1})
+            notSpecialTranscript = false
+            return
+        end
+    end
 end
